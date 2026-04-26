ca7d0c00f2cf1d1aab69d5788bae3cb7c9774a09
saremecin-hue <saremecin@gmail.com>
2026-04-26 20:38:07 +0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,5 @@
 export default async function handler(req, res) {
+  res.status(200).json({ test:"çaışıyor"})
   const { prompt, y1_analiz, y2_analiz, donem_bilgisi } = req.body;
 
   const response = await fetch("https://api.openai.com/v1/chat/completions", {
